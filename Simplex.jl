b6108745c741ffde659b25c9fdfa0bec7628b7f5
Renata Garcia <renata.garcia.eng@gmail.com>
2018-05-18 19:19:12 -0300

    Entregue, simplex2.

--- Simplex.jl
+++ Simplex.jl
@@ -1,27 +1,26 @@
 using JuMP
 using PyPlot
 using Optim
-using GLPKMathProgInterface
 
-debug = true
+####################################################
+###Implementação do SIMPLEX - 18/05/2018############
+###Renata Garcia Oliveira - Pós - 1712535###########
+###renata.garcia.eng@gmail.com######################
+
+debug = false
 epsilon = 1e-9
 max_iter = 1000
-fname = "simplex.log"
+filestream = open("simplex.log", "w")
 
 function printdbe(str::String)
     if (debug == true)
-        println(str)
+        printdbe(str)
     end
 end
 
-function printlog(filestream, str::String)
+function printlog(str::String)
     println(str)
     logWriteFile(filestream, (string(str, '\n')))
-end
-
-function logCreateFile()
-    filestream = open(fname, "w")
-    return filestream
 end
 
 function logWriteFile(filestream, str)
@@ -48,17 +47,15 @@ function def_prob2()
 end
 
 function def_prob3()
-    A = [3 3; 1 2; -1 -1]
+    A = [2 2; 1 2; -1 -1]
     b = [4; 4; -1]
     c = [4;3]
     return c, A, b
 end
 
-function simplexFaseI(c, A, b)
+function simplex(c, A, b)
 
-    f =  logCreateFile()
     num_folgas = size(b)[1]
-    println("num_folgas: $(num_folgas)")
     c = [c;zeros(num_folgas,1)]
     A = hcat(A, eye(num_folgas))
 
@@ -70,39 +67,105 @@ function simplexFaseI(c, A, b)
     ind_base = [(m+1):n...]
     ind_nobase = [1:m...]
 
-    if any(b .> 0)
-        printlog(f,"\nSimplex Fase II")
+    if all(b .> 0)
+        printlog("\nSimplex Fase II")
         printdbe("c: $(c)")
         printdbe("A: $(A)")
         printdbe("b: $(b)")
+	x,z,status = simplexFaseII(c, A, b, ind_base, ind_nobase)
     else
-        i = indmin(b)
-        ind_base[i] = n + m + 1
-        ind_nobase = vcat(nobase, n + i)
-
-        c = [c;-1]
-        A = hcat(A, -ones(m))
-
-        r1,r2,r3, ind_base_2,ind_nobase_2 = SimplexFaseII(f, c, A, b, ind_base, ind_nobase)
-
-        printlog(f,"\nSimplex Fase I")
-
-        i = indmax(ind_nobase_2)
-        ind_nobase_2 = deleteat!(ind_nobase_2,i)
-
-        x,z,status = SimplexFaseII(f, c, A, b,ind_base_2,ind_nobase_2)
-        printlog(f,"\nSimplex Fase II")
-    end
-
-    a = simplexFaseII(f, c, A, b, ind_base, ind_nobase)
+    	printlog("\nSimplex Fase I")
+        printdbe("A: $(A)")
+        printdbe("c: $(c)")
+        printdbe("b: $(b)")
+        ind_base,ind_nobase,status = simplexFaseI(c, A, b, ind_base, ind_nobase)
+	if status == -2
+            printdbe("Problema Inviável")
+            return -2
+        else
+            printlog("\nSimplex Fase II")
+            A = A[:,vcat(ind_base,ind_nobase)]
+            c = c[vcat(ind_base,ind_nobase)]
+	    printdbe("base: $(ind_base)")
+	    printdbe("nobase: $(ind_nobase)")
+	    x,z,status = simplexFaseII(c,A,b,ind_base,ind_nobase)
+        end
+    end    
 end
 
-function simplexFaseII(f, c, A, b, ind_base, ind_nobase)
+function simplexFaseI(c, A, b, ind_base, ind_nobase)
+
+    A = hcat(A,-1*ones(1,size(A)[1])')   
+    c = vcat(zeros(length(c)),-1)
+
+    m = size(A)[1] + 1
+    n = size(A)[2] - m
+
+    ind_nobase = vcat([i for i in 1:n],size(A)[2])
+    ind_base  = [i for i in (n+1):(n+m-1)]
+
+    B = A[:,ind_base]   
+    N = A[:,ind_nobase] 
+    xB = B \ b     
+
+    j          = length(ind_nobase) 
+    k          = indmin(xB)    
+    r_ind_base  = ind_base[k]        
+    r_ind_nobase = ind_nobase[j]      
+    ind_base[k]      = r_ind_nobase   
+    ind_nobase[j]      = r_ind_base  
+
+    printlog("B: $(B)")
+    printlog("xB: $(xB)")
+
+    for i = 1:max_iter
+
+        printdbe("Iteracao #$(i)")
+
+        B = A[:,ind_base]
+        N = A[:,ind_nobase]
+        xB = B \ b       
+        dB = -B \ N
+        cB = c[ind_base] 
+        cN = c[ind_nobase]
+        cR = (cN' + cB'*dB)'
+        j  = indmax(cR)     
+
+        x      = zeros(n+m)
+        x[ind_base]  = xB
+
+        printdbe("x     = $(x)")
+        printdbe("ind_base  = $(ind_base)")
+        printdbe("ind_nobase = $(ind_nobase)")
+        printdbe("")
+
+        if all(cR .<= epsilon)
+            ind_base
+            ind_nobase = ind_nobase[ind_nobase .!= size(A)[2]]
+            status = 1
+            return (ind_base, ind_nobase, status)
+        end
+
+        if all(dB[:,j] .>= epsilon)
+            status = -2
+            return (status);
+        end
+
+        r  = xB./(dB[:,j])
+        k  = indmax(r[r .< 0])
+
+        auxB  = ind_base[k]
+        ind_base[k] = ind_nobase[j]
+        ind_nobase[j] = auxB
+    end	
+end
+
+function simplexFaseII(c, A, b, ind_base, ind_nobase)
 
     for i=1:max_iter
-        printlog(f,"\niteração: $(i)")
-        printlog(f,"base: $(ind_base)")
-        printlog(f,"no base: $(ind_nobase)")
+        printlog("\niteração: $(i)")
+        printlog("base: $(ind_base)")
+        printlog("no base: $(ind_nobase)")
 
         B = A[:, ind_base]
         printdbe("B: $(B)")
@@ -117,10 +180,10 @@ function simplexFaseII(f, c, A, b, ind_base, ind_nobase)
         printdbe("xB: $(xB)")
         xw = zeros(size(A)[:2])
         xw[ind_base] = xB
-        printlog(f, "xw: $(xw)")
+        printlog("xw: $(xw)")
 
         z = cB'*xB
-        printlog(f,"z: $(z)")
+        printlog("z: $(z)")
         y = B'\cB
         printdbe("y: $(y)")
         cr = (cN' - y'*N)'
@@ -128,8 +191,8 @@ function simplexFaseII(f, c, A, b, ind_base, ind_nobase)
 
         if all(cr .<= epsilon)
             z = cB' * xB
-            println("Custo Total = ", z)
-            println("Variáveis Básicas / Valores = ", ind_base, "/", xB)
+            printdbe("Custo Total = $(z)")
+            printdbe("Variáveis Básicas / Valores = $(ind_base) / $(xB)")
             status=1
             return xB,z,status,ind_base,ind_nobase
         end
@@ -145,9 +208,9 @@ function simplexFaseII(f, c, A, b, ind_base, ind_nobase)
         printdbe("dall: $(dall)")
 
         if all(dall .>= 0)
-            printlog(f, "Problema Irrestrito")
-            printlog(f, "Custo total infinito")
-            printlog(f, "Direção extrema $(dall)")
+            printlog("Problema Irrestrito")
+            printlog("Custo total infinito")
+            printlog("Direção extrema $(dall)")
             return Inf, dall, -1
         end
         #se não há cr(i) não negativo
@@ -176,8 +239,27 @@ function simplexFaseII(f, c, A, b, ind_base, ind_nobase)
     return z, x, 1
 end
 
-c, A, b = def_prob3()
-ct, d, status = simplexFaseI(c, A, b)
+printlog("\n\n######SIMPLEX PROB 1########")
+c, A, b = def_prob1()
+ct, d, status = simplex(c, A, b)
 printdbe("ct: $(ct)")
 printdbe("d: $(d)")
 printdbe("status: $(status)")
+
+
+printlog("\n\n######SIMPLEX PROB 2########")
+c, A, b = def_prob2()
+ct, d, status = simplex(c, A, b)
+printdbe("ct: $(ct)")
+printdbe("d: $(d)")
+printdbe("status: $(status)")
+
+
+printlog("\n\n######SIMPLEX PROB 3########")
+c, A, b = def_prob3()
+ct, d, status = simplex(c, A, b)
+printdbe("ct: $(ct)")
+printdbe("d: $(d)")
+printdbe("status: $(status)")
+
+
